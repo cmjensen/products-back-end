558208454989fe0cb674b0598034bb9baa3da41c
Christie Jensen <christiejensen@Christies-MacBook-Air.local>
2021-02-08 20:46:51 -0700

    complete, all postman tests passed

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const massive = require('massive')
+const ctrl = require('./products_controller')
 require('dotenv').config()
 
 const app = express()
@@ -8,17 +9,22 @@ const { SERVER_PORT, CONNECTION_STRING } = process.env
 
 massive({
     connectionString: CONNECTION_STRING,
-    ssl: {
-        rejectUnauthorized: false
-    }
-}).then((db) => {
-    app.set('db', db)
-    console.log('db connected')
-}).catch(err => console.log(err))
-
-//ENDPOINTS
+    ssl: { rejectUnauthorized: false }
+    })
+        .then((db) => {
+            app.set('db', db)
+            console.log('db connected')
+        })
+        .catch(err => console.log(err))
 
 app.use(express.json())
+
+//ENDPOINTS
+app.get('/api/products', ctrl.getAll)
+app.get('/api/products/:id', ctrl.getOne)
+app.put('/api/products/:id', ctrl.update)
+app.post('/api/products', ctrl.create)
+app.delete('/api/products/:id', ctrl.delete)
 
 app.listen(SERVER_PORT, () => {
     console.log(`Server listening on port ${SERVER_PORT}`)
--- products_controller.js
+++ products_controller.js
@@ -1,30 +1,34 @@
-const { Update } = require("massive");
-
 module.exports = {
-    create: (req, res, next) => {
+    create: (req, res) => {
         const db = req.app.get('db')
+        const { name, description, price, image_url } = req.body
+
         db
-            .create_product()
-            .then(() => {
-                res.sendStatus(200)
-            }).catch( err => {
+            .create_product([ name, description, price, image_url ])
+            .then(() =>
+                res.sendStatus(200))
+            .catch( err => {
                 res.status(500).send({errorMessage: 'We are experiencing technical difficulties.'})
                 console.log(err)
             })
     },
-    getOne: (req, res, next) => {
+
+    getOne: (req, res) => {
         const db = req.app.get('db')
+        const { id } = req.params
+
         db
-            .read_product()
-            .then((product) => {
-                res.status(200).send(product)
-            }).catch( err => {
+            .read_product( id )
+            .then((product) => 
+                res.status(200).send(product))
+            .catch( err => {
                 res.status(500).send({errorMessage: 'We are experiencing technical difficulties.'})
                 console.log(err)
             })
     },
-    getAll: (req, res, next) => {
+    getAll: (req, res) => {
         const db = req.app.get('db')
+
         db
             .read_products()
             .then(( products ) => {
@@ -34,10 +38,12 @@ module.exports = {
                 console.log(err)
             })
     },
-    update: (req, res, next) => {
+    update: (req, res) => {
         const db = req.app.get('db')
+        const { params, query } = req
+
         db
-            .update_product()
+            .update_product( [params.id, query.desc] )
             .then(() => {
                 res.sendStatus(200)
             }).catch( err => {
@@ -45,15 +51,17 @@ module.exports = {
                 console.log(err)
             })
     },
-    delete: (req, res, next) => {
+    delete: (req, res) => {
         const db = req.app.get('db')
+        const { id } = req.params
+
         db
-            .delete_product()
-            .then(() => {
-                res.sendStatus(200)
-            }).catch( err => {
+            .delete_product( id )
+            .then(() =>
+                res.sendStatus(200))
+            .catch( err => {
                 res.status(500).send({errorMessage: 'We are experiencing technical difficulties.'})
                 console.log(err)
             })
     }
-}
+};
